ca84394f2c2f1f9d2fe3083df973e47d54ec69be
ahmet bilgic <ahmet_bilgic@yahoo.com>
2022-01-01 15:55:45 +0300

    Add project files.

---
+++ FunctionExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
