7a82267fedb725fdb7184769e08926dffeb56fac
ahmet bilgic <ahmet_bilgic@yahoo.com>
2022-01-01 16:24:21 +0300

    Function example

--- FunctionExample/Program.cs
+++ FunctionExample/Program.cs
@@ -6,7 +6,12 @@ namespace FunctionExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var func = new Func<int, int, int>(CalculateValue);
+            Console.WriteLine(func(2, 5));
+        }
+        public static int CalculateValue(int a, int b)
+        {
+            return a * b;
         }
     }
 }
